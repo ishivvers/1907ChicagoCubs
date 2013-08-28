6191fb2b6372c9931a48aa1dd0910f7690b490fe
Isaac Shivvers <ishivvers@berkeley.edu>
2013-08-28 16:02:58 -0700

    first attempt at parallel CV parameter fit

--- isaac/models.py
+++ isaac/models.py
@@ -4,17 +4,25 @@ A library of models and model helper functions for the
 '''
 
 from datalib import *
-from sklearn import metrics
-from sklearn import linear_model
-from sklearn.cross_validation import train_test_split
+from sklearn import linear_model, grid_search
+from multiprocessing import cpu_count
 
 
 '''
+################################################################################################
 # example: fit a model to a few of the variables (each variable averaged across models and hours)
+
 trainX = load_GEFS('train', variables=['dlwrf_sfc','dswrf_sfc','tcdc_eatm','tmp_2m'])
 times,trainY = load_MESONET('train.csv')
 testX = load_GEFS('test', variables=['dlwrf_sfc','dswrf_sfc','tcdc_eatm','tmp_2m'])
 model = RunLassoCV( trainX, trainY, testX )
+# display the importance of various variables
+plt.figure(figsize=(14,4))
+plt.imshow(model.coef_)
+plt.xlabel('feature')
+plt.ylabel('model')
+plt.show()
+
 '''
 def RunLassoCV( trainX, trainY, testX, verbose=True, save=True ):
     # use the built-in cross-validation routine to 
@@ -32,5 +40,34 @@ def RunLassoCV( trainX, trainY, testX, verbose=True, save=True ):
     if verbose: print '\nComplete.'
     
     return model
+
+'''
+trainX = load_GEFS('train', variables=['dlwrf_sfc','dswrf_sfc','tcdc_eatm','tmp_2m'])
+times,trainY = load_MESONET('train.csv')
+testX = load_GEFS('test', variables=['dlwrf_sfc','dswrf_sfc','tcdc_eatm','tmp_2m'])
+model = RunLassoCVParallel( trainX, trainY, testX, np.logspace(-1,4,50) )
+# display the importance of various variables
+plt.figure(figsize=(14,4))
+plt.imshow(model.coef_)
+plt.xlabel('feature')
+plt.ylabel('model')
+plt.show()
+'''
+def RunLassoCVParallel( trainX, trainY, testX, alphas, verbose=True, save=True ):
+    '''
+    Run a || grid search for optimal parameters.
+    '''
+    if verbose: print '\nChoosing best alpha and fitting model','#'*20,'\n'
+    model = linear_model.Lasso(normalize=True)
+    cvs = grid_search.GridSearchCV(model, {'alpha':alphas}, n_jobs=cpu_count()/2, verbose=int(verbose))
+    cvs.fit( trainX, trainY )
     
+    if verbose: print '\nProducing estimates','#'*20,'\n'
+    predictions = model.predict( testX )
+    if save: save_submission( predictions, 'LassoCV_submission.csv' )
+    
+    if verbose: print '\nComplete.'
+    
+    return model
+
     
