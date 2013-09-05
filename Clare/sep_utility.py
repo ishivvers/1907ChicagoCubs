3ae90214869a8e602755e8494aaf50e5deba4078
Clare Saunders <cmsaunders@berkeley.edu>
2013-09-04 23:02:48 -0700

    Added a new fitter (Random Forest)

--- Clare/sep_utility.py
+++ Clare/sep_utility.py
@@ -7,11 +7,26 @@ def evaluate( model, data ):
     s and e are the number of sites and events (days)
     """
     
-    s = len(data)
-    try:
-        e = len(data[0])
-    except:
-        e = 1.0
-    abs_dif = abs(data - model)
+    s,e = data.shape
 
-    return sum( abs_dif)/(s*e)
+    abs_dif = abs(data - model)
+    
+    return np.sum( abs_dif)/(s*e)
+
+def create_2d_interpolation(f, day, hour, ens=0):
+    '''
+    Returns a 2d spline function F(x,y) for the variable in the GEFS file, where
+    x = lon and y = lat.
+    f: a netCDF4 Dataset object created from a GEFS file.
+    day: index of the day to use (labeled "time")
+    hour: index of the hour to use (labeled "fhour")
+    ens: index of the ensemble member to use (labeled "ens")
+    '''
+    var = f.variables.keys()[-1]             # the name of the variable encoded in f
+    dd = np.where( f.variables['intTime'][:] == day)[0]
+    hh = np.where( f.variables['fhour'][:] == hour )[0]
+    arr = f.variables[var][dd,ens,hh,:,:] # the variable array, accessed as var[...]
+    x = f.variables['lon'][:]-360
+    y = f.variables['lat'][:]
+    F = interp2d(x,y,arr, kind='linear', bounds_error=True)
+    return F
